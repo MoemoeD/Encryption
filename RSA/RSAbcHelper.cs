a6fde4d75ffbc1ac5969c1a7ff88e38d45f2192d
MoemoeD <37505963+MoemoeD@users.noreply.github.com>
2019-01-31 15:04:56 +0800

    BC

---
+++ RSA/RSAbcHelper.cs
@@ -0,0 +1,117 @@
+﻿using Org.BouncyCastle.Asn1;
+using Org.BouncyCastle.Asn1.Pkcs;
+using Org.BouncyCastle.Asn1.X509;
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Encodings;
+using Org.BouncyCastle.Crypto.Engines;
+using Org.BouncyCastle.Crypto.Generators;
+using Org.BouncyCastle.Crypto.Parameters;
+using Org.BouncyCastle.Math;
+using Org.BouncyCastle.Pkcs;
+using Org.BouncyCastle.Security;
+using Org.BouncyCastle.X509;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RSA
+{
+    public class RSAbcHelper
+    {
+        public struct key
+        {
+            public string publicKey { get; set; }
+
+            public string privateKey { get; set; }
+        }
+
+        public key GetKey()
+        {
+            RsaKeyPairGenerator keyPairGenerator = new RsaKeyPairGenerator();
+            RsaKeyGenerationParameters param = new RsaKeyGenerationParameters(BigInteger.ValueOf(3), new SecureRandom(), 1024, 25);
+            keyPairGenerator.Init(param);
+            AsymmetricCipherKeyPair keyPair = keyPairGenerator.GenerateKeyPair();
+            AsymmetricKeyParameter publicKey = keyPair.Public;
+            AsymmetricKeyParameter privateKey = keyPair.Private;
+            SubjectPublicKeyInfo subjectPublicKeyInfo = SubjectPublicKeyInfoFactory.CreateSubjectPublicKeyInfo(publicKey);
+            PrivateKeyInfo privateKeyInfo = PrivateKeyInfoFactory.CreatePrivateKeyInfo(privateKey);
+
+            Asn1Object asn1ObjectPublic = subjectPublicKeyInfo.ToAsn1Object();
+            byte[] publicInfoByte = asn1ObjectPublic.GetEncoded("UTF-8");
+            Asn1Object asn1ObjectPrivate = privateKeyInfo.ToAsn1Object();
+            byte[] privateInfoByte = asn1ObjectPrivate.GetEncoded("UTF-8");
+
+            return new key()
+            {
+                publicKey = Convert.ToBase64String(publicInfoByte),
+                privateKey = Convert.ToBase64String(privateInfoByte)
+            };
+        }
+
+        private AsymmetricKeyParameter GetPublicKeyParameter(string s)
+        {
+            s = s.Replace("\r", "").Replace("\n", "").Replace(" ", "");
+            byte[] publicInfoByte = Convert.FromBase64String(s);
+            AsymmetricKeyParameter pubKey = PublicKeyFactory.CreateKey(publicInfoByte);
+            return pubKey;
+        }
+
+        private AsymmetricKeyParameter GetPrivateKeyParameter(string s)
+        {
+            s = s.Replace("\r", "").Replace("\n", "").Replace(" ", "");
+            byte[] privateInfoByte = Convert.FromBase64String(s);
+            // Asn1Object priKeyObj = Asn1Object.FromByteArray(privateInfoByte);//这里也可以从流中读取，从本地导入   
+            // PrivateKeyInfo privateKeyInfo = PrivateKeyInfoFactory.CreatePrivateKeyInfo(privateKey);
+            AsymmetricKeyParameter priKey = PrivateKeyFactory.CreateKey(privateInfoByte);
+            return priKey;
+        }
+
+        public string EncryptByPrivateKey(string s, string key)
+        {
+            //非对称加密算法，加解密用  
+            IAsymmetricBlockCipher engine = new Pkcs1Encoding(new RsaEngine());
+
+
+            //加密  
+
+            try
+            {
+                engine.Init(true, GetPrivateKeyParameter(key));
+                byte[] byteData = System.Text.Encoding.UTF8.GetBytes(s);
+                var ResultData = engine.ProcessBlock(byteData, 0, byteData.Length);
+                return Convert.ToBase64String(ResultData);
+                //Console.WriteLine("密文（base64编码）:" + Convert.ToBase64String(testData) + Environment.NewLine);
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+
+            }
+        }
+        public string DecryptByPublicKey(string s, string key)
+        {
+            s = s.Replace("\r", "").Replace("\n", "").Replace(" ", "");
+            //非对称加密算法，加解密用  
+            IAsymmetricBlockCipher engine = new Pkcs1Encoding(new RsaEngine());
+
+
+            //解密  
+
+            try
+            {
+                engine.Init(false, GetPublicKeyParameter(key));
+                byte[] byteData = Convert.FromBase64String(s);
+                var ResultData = engine.ProcessBlock(byteData, 0, byteData.Length);
+                return System.Text.Encoding.UTF8.GetString(ResultData);
+
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+
+            }
+        }
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RSA;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RSAbcHelper h = new RSAbcHelper();
+            h.GetKey();
+        }
+    }
+}
